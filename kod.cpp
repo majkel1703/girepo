1234c82753652cf0ab769d2f802ce2d788689df8
Michal Florkiweicz <majkel1703@o2.pl>
2017-10-01 14:23:23 +0200

    Pole kola - poprawki

--- kod.cpp
+++ kod.cpp
@@ -1,15 +1,20 @@
 #include <cstdlib>
 #include <iostream>
+#include <math.h>
+
 using namespace std;
 
 int main(int arge, char *argv[])
 {
-    int PI, R;
-    int pole_kola;
-    PI = 3;
+    double pole_kola, PI, R;
+
+    PI = 3.14;
     cout << "podaj promien ko³a: ";
     cin >> R;
     pole_kola = PI * R * R;
-    cout << "pole kola o promieniu R wynosi" + pole_kola << endl <<endl;
+    cout << "pole kola o promieniu R wynosi: ";
+    cout << pole_kola << endl;
+
+    system("PAUSE");
 }
 
