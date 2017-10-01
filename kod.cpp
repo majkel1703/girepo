116f4ad799564a8a8df88bd68b47b08149cf5cea
Michal Florkiweicz <majkel1703@o2.pl>
2017-10-01 14:16:15 +0200

    Pole kola

--- kod.cpp
+++ kod.cpp
@@ -1,10 +1,15 @@
+#include <cstdlib>
 #include <iostream>
-// moj program
 using namespace std;
 
-int main()
+int main(int arge, char *argv[])
 {
-    cout<<"Hello World!";
-    cin.get();
-    return 0;
+    int PI, R;
+    int pole_kola;
+    PI = 3;
+    cout << "podaj promien ko³a: ";
+    cin >> R;
+    pole_kola = PI * R * R;
+    cout << "pole kola o promieniu R wynosi" + pole_kola << endl <<endl;
 }
+
