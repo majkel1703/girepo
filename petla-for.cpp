ccc4f1b710523038c8d23cceec555d23708b6f85
Michal Florkiweicz <majkel1703@o2.pl>
2017-10-02 21:11:56 +0200

    kodzik_suma10liczb

--- petla-for.cpp
+++ petla-for.cpp
@@ -6,17 +6,15 @@ using namespace std;
 int main(int argc, char **argv)
 {
     int i;
-    int j;
-    for(i =2; i < 100; i+=2)
-    {
-        cout << '*';
-        if ( i % 10 == 0 )
-        {
-            for (j=0; j<9; j++)
-            cout << '#';
-            cout << endl;
-    }
+    int wynik=0;
+
+    for(i =1; i < 11; i++)
+{
+    wynik+=i;
+
 }
+cout << wynik;
+
 	return 0;
 }
 
