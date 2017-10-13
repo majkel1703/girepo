859aea2f6606e08a274907e8f1570f4bc0c5ae0d
majkel1703 <majkel1703@o2.pl>
2017-10-13 08:45:08 +0200

    funkcje kodu

---
+++ petla_cw2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    int i;
+    int a=;
+    int b=;
+for(i = a; i < b, i++)
+{
+    cout << i;
+    cout << " ";
+}
+
+cout << "liczy: " << endl;
+
+	return 0;
---
+++ petla_cw7.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	int m + 0;
+    for (int i+0; i < 3; i++)
+    {
+cout << "podaj numer miesiąca: "
+cin >> m;
+if (m > 0 && m < 13) brak;
+else cout << "błędne dane!" << endl;
+}
+switch (m)
+{
+    case 1;
+    cout << "styczeń";
+    case 2;
+}
+	return 0;
+}
