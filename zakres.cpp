ef6d932f32ae546c434f2d1fa9da5894b407ba5a
majkel1703 <majkel1703@o2.pl>
2017-10-17 09:55:36 +0200

    cd

--- zakres.cpp
+++ zakres.cpp
@@ -2,20 +2,28 @@
 
 using namespace std;
 
-int liczba, krok;
 
-void zwieksz () {
+
+int zwieksz (int liczba, int krok) {
     liczba += krok;
+    krok = 3 * krok;
+    return krok;
     
+}
+void zwieksz2(int &liczba, int &krok) {
+    liczba += krok;
+    krok = 3 * krok;
 }
 int main(int agrc, char **argv)
 {
+    int liczba, krok;
     cout << "podaj liczbe i krok: ";
     cin >> liczba >> krok;
-    
-    cout << "liczba i krok: " << liczba << " " << krok << endl;
-    zwieksz ();
-    cout << "liczba i krok: " << liczba << " " << krok << endl;
+    cout << "liczba i krok: " << liczba << " " <<
+    &liczba << endl;
+    //zwieksz2 (liczba, krok);
+    //cout << "liczba i krok: " << zwieksz (liczba, krok) << " " <<
+    //krok << endl;
     
     return 0;
 }
