7db7be393b0808a9bb87916fefa2850b0d453328
majkel1703 <majkel1703@o2.pl>
2017-11-06 12:38:02 +0100

    costam

---
+++ python/dlotek.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import random
+
+def losuj(ileliczb, maksliczb):
+    liczby = []
+
+def main(args):
+    ileliczb = int(input("ile liczb zgadywać?"))
+    maksliczb = int(input("maksymalna losowa liczba: "))+
+
+
+    liczby = []
+
+    ile = 0
+
+    while ile < ileliczb
+        liczba = random.randint(0, maksliczb)
+        if liczby.count(liczba) == 0;
+            liczby,append(liczba)
+         ile =+ 1
+    print (ileliczb, maksliczb)
+
+    for i in range(ileliczb):
+        random. randint(0, maksliczb)
+
+    return 0
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
