a30cd141a04983ff7f735be301aeedac20e516c2
majkel1703 <majkel1703@o2.pl>
2017-11-21 10:36:01 +0100

    b

--- python/euklidesa.py
+++ python/euklidesa.py
@@ -2,21 +2,19 @@
 # -*- coding: utf-8 -*-
 
 
-def nww_v1(a, b):
-    while a != b:
-        if a > b:
-            a = a - b
-        else:
-            b = b - a
-    return a
+def NWD_v1(a, b):
+    while a > 0:
+        a = b % b
+        b = b - a
+        return b
 
 
 def main(args):
     a = int(input("podaj liczbe naturalną: "))
     b = int(input("podaj kolejną liczbą naturalną: "))
-    assert nww_v1(5, 10) == 5
-    assert nww_v1(11, 33) == 11
-    print("NWW({:d}, {:d}) = {:d}".format(a, b, nww_v1(a, b)))
+    assert NWD_v1(5, 10) == 5
+    assert NWD_v1(11, 33) == 11
+    print("NWD({:d}, {:d}) = {:d}".format(a, b, NWD_v1(a, b)))
     return 0
 
 
